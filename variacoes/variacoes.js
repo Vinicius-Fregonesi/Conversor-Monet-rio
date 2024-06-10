739042414a8f9d5e3f4bd20a6c218a47e4cdd5b4
Vinicius-Fregonesi <viniciusfregone@gmail.com>
2024-06-10 13:22:19 -0300

    Ajustes no grafico

--- variacoes/variacoes.js
+++ variacoes/variacoes.js
@@ -100,7 +100,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 scales: {
                     x: {
                         grid: {
-                          color: 'red',
+                          color: 'green',
                           borderColor: 'grey',
                           tickColor: 'grey'
                         }
